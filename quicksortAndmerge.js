b3b87c2f24df093ed7ec635e99d23fbde5cc9c9f
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-08-02 12:30:11 -0400

    merge sort problem finished

--- quicksortAndmerge.js
+++ quicksortAndmerge.js
@@ -1,5 +1,5 @@
 'use strict';
-
+let increment = 0;
 function quickSort(array, start=0, end=array.length){
 
 
@@ -8,8 +8,12 @@ function quickSort(array, start=0, end=array.length){
   if(start >= end){
     return array;
   }
+
   const middle = partition(array, start, end);
+  increment++;
+  console.log(increment);
   array = quickSort(array, start, middle);
+
   array = quickSort(array, middle + 1, end);
   return array;
 
@@ -34,6 +38,49 @@ function partition(array, start, end) {
     return j;
 };
 
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    let left = array.slice(0, middle);
+    let right = array.slice(middle, array.length);
+    increment++;
+    console.log(increment);
+    left = mergeSort(left);
+    right = mergeSort(right);
+    return merge(left, right, array);
+};
+
+function merge(left, right, array) {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    let outputIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            array[outputIndex++] = left[leftIndex++];
+        }
+        else {
+            array[outputIndex++] = right[rightIndex++];
+        }
+    }
+
+    for (let i=leftIndex; i<left.length; i++) {
+        array[outputIndex++] = left[i];
+    }
+
+    for (let i=rightIndex; i<right.length; i++) {
+        array[outputIndex++] = right[i];
+    }
+    return array;
+};
+
+
+
+
+
+
 const Array = `89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5`;
 
 
@@ -47,4 +94,6 @@ function convert(arr){
 
 let converter = convert(Array);
 
-console.log(quickSort(converter));
+//console.log(quickSort(converter));
+
+console.log(mergeSort(converter));
